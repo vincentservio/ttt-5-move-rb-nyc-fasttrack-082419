e718828cfeee2e4975dbc95c9943ce5ee21c054e
vincent servio <vincentservio@gmail.com>
2019-08-25 14:04:58 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board (board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -9,7 +9,7 @@ end
 def input_to_index(input)
   input.to_i - 1
   
-
+end
 
 def move (board, position, char= "X")
   board[position] = char
